5850d87ba9282c6545543730c464e453a9a853a9
straysheep-dev <bah@straysheep.dev>
2022-07-05 13:40:40 -0700

    Update gsettings

--- gsettings.sh
+++ gsettings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# gsettings / dconf config
-# must be run separetly as UID1000/normal user
+# gsettings
+# must be run as UID100x/normal user, sudo applies to root
 
 MAJOR_UBUNTU_VERSION=$(grep VERSION_ID /etc/os-release | cut -d '"' -f2 | cut -d '.' -f 1)
 
@@ -30,14 +30,6 @@ gsettings set org.gnome.desktop.media-handling autorun-never 'true'
 gsettings set org.gnome.desktop.media-handling automount false
 gsettings set org.gnome.desktop.media-handling automount-open false
 
-# See /usr/share/glib-2.0/schemas/org.gnome.Terminal.gschema.xml
-# See dconf list /org/gnome/terminal
-# See dconf read /org/gnome/terminal/[..]/next-tab
-# Some settings require a literal path
-# Sets the hotkey combination of Ctrl+Shift+Left/Right Arrows to change terminal tabs
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Left'
-gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary><Shift>Right'
-
 # Show hidden files and folders
 gsettings set org.gtk.Settings.FileChooser show-hidden 'true'
 
@@ -58,3 +50,28 @@ fi
 # Crash reports still generate under /var/crash/ to review if apport enabled
 gsettings set org.gnome.desktop.privacy report-technical-problems 'false'
 gsettings set org.gnome.desktop.privacy send-software-usage-stats 'false'
+
+# See /usr/share/glib-2.0/schemas/org.gnome.Terminal.gschema.xml
+# See dconf list /org/gnome/terminal
+# See dconf read /org/gnome/terminal/[..]/next-tab
+# Some settings require a literal path
+# Sets the hotkey combination of Ctrl+Shift+Left/Right Arrows to change terminal tabs
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ prev-tab '<Primary><Shift>Left'
+gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ next-tab '<Primary><Shift>Right'
+
+# You need to double quote the settings within brackets "[]" when setting keybinding keys via CLI like in this script
+# Set switching workspaces to Ctrl + Super + < ^ v >
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['<Primary><Super>Right']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['<Primary><Super>Left']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-down "['<Primary><Super>Down']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-up "['<Primary><Super>Up']"
+
+# Set moving applications to Ctrl + Super + Shift + < ^ v >
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-down "['<Primary><Super><Shift>Down']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-left "['<Primary><Super><Shift>Left']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-right "['<Primary><Super><Shift>Right']"
+gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-up "['<Primary><Super><Shift>Up']"
+
+# Ensure switching applications is set to [Shift + ]Super + Tab, typically the default
+org.gnome.desktop.wm.keybindings switch-applications-backward "['<Super><Shift>Tab']"
+org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
